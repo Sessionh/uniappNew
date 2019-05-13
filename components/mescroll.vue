a074e3f60916b584464fe8e90230421424f91e04
huyh <huyh@51kezhi.com>
2019-05-13 18:12:47 +0800

    修改下拉刷新

--- components/mescroll.vue
+++ components/mescroll.vue
@@ -32,20 +32,22 @@ export default {
 				use: true, // 是否启用上拉加载; 默认true
 				auto: true, // 是否在初始化完毕之后自动执行上拉加载的回调; 默认true
 				isLock: false, // 是否锁定上拉加载 (可用于不触发upCallback,只保留回到顶部按钮的场景)
-				callback: this.upCallback, 
+                callback: this.upCallback, 
 				page: {
 					num: 0, // 当前页码,默认0,回调之前会加1,即callback(page)会从1开始
 					size: 10 // 每页数据的数量,默认10
 				},
-				toTop: {
-                    src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAclBMVEUAAAAAAAAAAAAAAADp6ekAAAAAAAAAAADOzs4AAAAAAAAAAAAAAAAAAAAAAAAAAADo6OjW1tYeHh7l5eXh4eHY2NjAwMC5ubm3t7cxMTH7+/vv7+/s7Ozc3NzNzc2tra0TExPp6enS0tLQ0NDDw8P///9ezvBLAAAAJXRSTlNNAAod1T46NbASSEc0Kykk07pTzsm9o52ZWfXg2sGvk1HUtbOmfFE0sAAAAT1JREFUSMeV1t1ygjAQhuGvu4ZU+VUUqVrtX+7/FpuCTJEsmH2POMgzMEzYgBdlISAurEmB1NiC6RkgzjYYtcmYFgCVBkGmpDnACcQSFgHlmC2nEKwtFrLrKVgnWCwZBKT1shgDsniapRHIEVH+DxhR8QAoiQMJ3UGJyMoekIkFhjrAiI47kGHU2U06Y1T2B+hhP7evfdfr/aJ92O3kAUMRe1BA6K3e7yBUeGCl9ZVz7x8Isx4Ycf3pJArjQYppu9rd6qpxhxCkHoTr9+6Gzwo/bhsKD4T1LbBaAUdBCI90cEf0wIsv4ZHMFDQYAJrpLYz8Wgcgv9ZCAwoPWAO423zxYEPd9o4HWf8BxQPuAJlYYGh+CFwuCCsXxsz2Wxoz2kGmHZXqYawe9/oDRX9k6Q9F/bGrP9j1vw76nxNlv9R2ECcaFzswAAAAAElFTkSuQmCC', // 回到顶部按钮的图片路径,支持网络图
-					offset: 100
-                },
+				
 				noMoreSize: 3, // 配置列表的总数量要大于等于5条才显示'-- END --'的提示
 				empty: {
 					tip: '暂无相关数据'
-				}
+				},
+				toTop: {
+				    src: '../static/mescroll-totop.png', // 回到顶部按钮的图片路径,支持网络图
+					offset: 300
+				},
 			},
+			
 			// 列表数据
 			dataList: []
 		}
@@ -58,7 +60,7 @@ export default {
 	onPageScroll(e) {
 		this.mescroll && this.mescroll.onPageScroll(e);
 	},
-	methods: {
+    methods: {
         setText() {
 			this.hasSetText = !this.hasSetText;
 			uni.setNavigationBarTitle({
