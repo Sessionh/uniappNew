4f0da85918aa3f970f4113ae6dddfed9b96f5fab
huyh <huyh@51kezhi.com>
2020-03-06 18:12:28 +0800

    提交

--- common/mButton.vue
+++ common/mButton.vue
@@ -2,15 +2,26 @@
 	<div class="my-button" :style="{background: bgColor}" @click="click">
 
 
-		<!-- <view class="sk-wave" v-if="isLoad">
+		<view class="sk-wave" v-if="isLoad && loadType === 'wave'">
 			<view class="sk-wave-rect" :class="iconColor"></view>
 			<view class="sk-wave-rect" :class="iconColor"></view>
 			<view class="sk-wave-rect" :class="iconColor"></view>
 			<view class="sk-wave-rect" :class="iconColor"></view>
 			<view class="sk-wave-rect" :class="iconColor"></view>
-		</view> -->
+		</view>
 		
-		<view class="spinner">
+		<view class="spinner"  v-if="isLoad && loadType === 'spinner'">
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
+			<view class="spinner-item"></view>
 			<view class="spinner-item"></view>
 		</view>
 		
@@ -45,6 +56,10 @@
 			text: {
 				type: String,
 				default: ''
+			},
+			loadType: {
+				type: String,
+				default: 'spinner'
 			}
 		},
 		data() {
@@ -84,30 +99,90 @@
 
 	}
 	
+	/* spinner */
 	.spinner {
 		position: relative;
 		display: inline-block;
-		width: 30px;
+		width: 60rpx;
 		max-width: 100%;
-		height: 30px;
+		height: 60rpx;
 		max-height: 100%;
 		vertical-align: middle;
 		animation: van-rotate 0.8s linear infinite;
-	
-		
+		animation-timing-function: steps(12);
 	}
+	
 	.spinner-item {
 		position: absolute;
 		top: 0;
 		left: 0;
 		width: 100%;
 		height: 100%;
+  }
+	.spinner-item::before {
+		display: block;
+		width: 4rpx;
+		height: 25%;
+		margin: 0 auto;
+		background-color: #999;
+		border-radius: 40%;
+		content: ' ';
+		
 	}
 	.spinner-item:nth-of-type(1) {
 		transform: rotate(30deg);
 		opacity: 1;
 		
 	}
+	.spinner-item:nth-of-type(2) {
+		transform: rotate(60deg);
+		opacity: 0.9375;
+	}
+	.spinner-item:nth-of-type(3) {
+		transform: rotate(90deg);
+		opacity: 0.875;
+	}
+	.spinner-item:nth-of-type(4) {
+		transform: rotate(120deg);
+		opacity: 0.8125;
+	}
+	.spinner-item:nth-of-type(5) {
+		transform: rotate(150deg);
+		opacity: 0.75;
+	}
+	.spinner-item:nth-of-type(6) {
+		transform: rotate(180deg);
+		opacity: 0.6875;
+	}
+	
+	.spinner-item:nth-of-type(7) {
+		transform: rotate(210deg);
+		opacity: 0.625;
+	}
+	.spinner-item:nth-of-type(8) {
+		transform: rotate(240deg);
+		opacity: 0.5625;
+	}
+	.spinner-item:nth-of-type(9) {
+		transform: rotate(270deg);
+		opacity: 0.5;
+		
+	}
+	.spinner-item:nth-of-type(10) {
+		transform: rotate(300deg);
+		opacity: 0.4375;		
+	}
+	
+	.spinner-item:nth-of-type(11) {
+		transform: rotate(330deg);
+		opacity: 0.375;
+	}
+	.spinner-item:nth-of-type(12) {
+		transform: rotate(360deg);
+		opacity: 0.3125;
+	}
+	
+	
 	@keyframes van-rotate {
 		0% {
 			transform: rotate(0deg)
@@ -120,7 +195,9 @@
 	
 	
 	
-
+/*
+ sk-wave
+*/
 	.sk-wave {
 		/* width: 60rpx; */
 		height: 62rpx;
